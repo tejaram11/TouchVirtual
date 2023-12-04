c365c2158646f278b0576d68973c259d144d8284
tejaram11 <tejarammuramalla11@gmail.com>
2023-12-04 22:41:38 +0530

    new interface

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,Response
+from flask import Flask,render_template,Response, stream_with_context
 from hand_roi import detector
 
 
@@ -16,9 +16,18 @@ def gen(detect):
         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         
 
-         
+@app.route("/video")
+def stream():
+    return render_template("video.html")
+
+@app.route("/video_frame")
+def video_frame():
+    return Response(stream_with_context(gen(detect)), mimetype='multipart/x-mixed-replace;boundary=frame')
+
+'''        
 @app.route("/home")
 def start():
     return Response(gen(detect),mimetype='multipart/x-mixed-replace;boundary=frame')
+'''
 
-app.run()
+app.run(debug=True)
--- hand_roi.py
+++ hand_roi.py
@@ -11,17 +11,19 @@ import time
 import pyautogui
 pyautogui.FAILSAFE=False
 
+
+
 hands= mp.solutions.hands.Hands()
 drawing_obj=mp.solutions.drawing_utils
 
 
 class detector():
     def __init__(self):
-        self.cap=cv2.VideoCapture(1)
+        self.cap=cv2.VideoCapture(0)
         
     def __del__(self):
         self.cap.release()
-        
+        cv2.destroyAllWindows()
         
     def hand_detector(self):
         screen_w, screen_h= pyautogui.size()
@@ -30,13 +32,13 @@ class detector():
         frame=cv2.flip(frame,1)
         time_start=0
         time_end=0
-        self.roi_w=1280
-        self.roi_h=720
-        self.roi_x=50
-        self.roi_y=50
+        self.roi_w=600
+        self.roi_h=400
+        self.roi_x=25
+        self.roi_y=35
         #extracting ROI
         frameRGB=frame[self.roi_y:self.roi_y+self.roi_w,self.roi_x:self.roi_x+self.roi_h]
-        frameRGB=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        frameRGB=cv2.cvtColor(frameRGB,cv2.COLOR_BGR2RGB)
         frame_h,frame_w,_=frameRGB.shape
         results=hands.process(frameRGB).multi_hand_landmarks
         
@@ -52,30 +54,36 @@ class detector():
                 #print(hand_coord)
                 
                 for id, landmark in enumerate(landmarks):
-                    x = int(landmark.x*frame_w)+self.roi_x
-                    y = int(landmark.y*frame_h)+self.roi_y
+                    x = int(landmark.x*self.roi_w)#+self.roi_x
+                    y = int(landmark.y*self.roi_h)#+self.roi_y
                     print(x, y)
                     if id == 8:
-                        cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))                      
+                                              
                         index_x = screen_w/frame_w*x
                         index_y = screen_h/frame_h*y
                         pyautogui.moveTo(index_x,index_y,0.2)
+                        
+                        cv2.circle(img=frame, center=(x,y), radius=9, color=(0,255,255))
                     if id == 4:
-                        cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
+                        
                         thumb_x = screen_w/frame_w*x
                         thumb_y = screen_h/frame_h*y
                         print('outside ',abs(index_y - thumb_y))
-                        if abs(index_y - thumb_y)<20:
-                            pyautogui.click(clicks=2)
+                        if abs(index_y - thumb_y)<5 or abs(index_x - thumb_x)<5:
+                            pyautogui.click(index_x,index_y,button="right")
                             pyautogui.sleep(1)
+                            cv2.putText(frame, "click", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0,255), 2)
+                        cv2.circle(img=frame, center=(x,y), radius=5, color=(0,0,255))
                     if id == 12:
-                        cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
+                        
                         mid_x = screen_w/frame_w*x
                         mid_y = screen_h/frame_h*y
                         print('outside ',abs(index_y - mid_y))
-                        if abs(index_y - mid_y)<20:
-                            pyautogui.click(clicks=2)
+                        if abs(index_y - mid_y)<5 or abs(index_x - mid_x)<5:
+                            pyautogui.click(index_x,index_y)
                             pyautogui.sleep(1)
+                            cv2.putText(frame, 'leftclick', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                        cv2.circle(img=frame, center=(x,y), radius=5, color=(0,255,0))
                 
                 
                 
@@ -86,4 +94,5 @@ class detector():
         cv2.rectangle(frame,(self.roi_x,self.roi_y),(self.roi_x+self.roi_w,self.roi_y+self.roi_h),(255,0,0),3)
         
         _,jpg=cv2.imencode('.jpg',frame)
+        
         return jpg.tobytes()
